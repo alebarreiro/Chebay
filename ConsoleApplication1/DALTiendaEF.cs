263abe6f4449fa75e2abc264b7ed87945849cdb1
AlejandroAnon <alejandroanonmallo@gmail.com>
2015-04-27 14:25:03 -0300

    Mas cosas de DAL Tienda

--- ConsoleApplication1/DALTiendaEF.cs
+++ ConsoleApplication1/DALTiendaEF.cs
@@ -135,6 +135,259 @@ namespace DataAccessLayer
                 }
             }
         }
-    
+
+        bool AgregarTienda(Tienda t)
+        //Completa el nombre, descripción, una URL (TiendaID).
+        //Devuelve FALSE si ya existe una Tienda con la misma URL.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    var query = from tienda in context.tiendas
+                                where tienda.TiendaID == t.TiendaID
+                                select tienda;
+                    if (query != null)
+                    {
+                        context.tiendas.Add(t);
+                        context.SaveChanges();
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        void ActualizarTienda(Tienda t)
+        //Cambiar nombre o descripción de t.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    var query = from tienda in context.tiendas
+                                where tienda.TiendaID == t.TiendaID
+                                select tienda;
+                    if (query != null)
+                    {
+                        Tienda tnd = query.FirstOrDefault();
+                        tnd.descripcion = t.descripcion;
+                        tnd.nombre = tnd.nombre;
+                        context.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        bool CambiarURLTienda(string idTienda, string nuevaURL)
+        //Cambia la URL de idTienda por nuevaURL.
+        //Devuelve FALSE si ya existe una tienda con URL nuevaURL.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    var query = from t in context.tiendas
+                                where t.TiendaID == idTienda
+                                select t;
+                    var nueva = from n in context.tiendas
+                                where n.TiendaID == nuevaURL
+                                select n;
+
+                    if (query != null && nueva == null)
+                    {
+                        Tienda tnd = query.FirstOrDefault();
+                        tnd.TiendaID = nuevaURL;
+                        context.SaveChanges();
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        Tienda ObtenerTienda(string idTienda)
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+        List<Tienda> ObtenerTodasTiendas()
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+        Tienda ObtenerTiendasAdministrador(string idAdministrador)
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+        void AgregarAdminTienda(string idAdministrador, string idTienda)
+        //Agrega a idAdministrador a idTienda.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        //CU: 1.2 INGRESAR CATEGORIA Y 1.3 ALTA CATEGORIA
+        bool AgregarCategoriaCompuesta(string idCategoria, string idPadre)
+        //idPadre no puede ser null. La categoría raiz se crea cuando se crea la tienda.
+        //Devuelve FALSE si ya existe una Categoria con el mismo nombre o no existe el padre.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    var existeCat = from c in context.categorias
+                                    where c.CategoriaID == idCategoria
+                                    select c;
+                    if (existeCat != null)
+                        return false;
+                    else
+                    {
+                        var existePadre = from p in context.categorias
+                                          where p.CategoriaID == idPadre
+                                          select p;
+                        if (existePadre == null)
+                            return false;
+                        else
+                        {
+                            CategoriaCompuesta padre = (CategoriaCompuesta) existePadre.FirstOrDefault();
+                            CategoriaCompuesta ret = new CategoriaCompuesta();
+                            ret.CategoriaID = idCategoria;
+                            ret.padre = padre;
+                            ret.hijas = new List<Categoria>();
+                            ret.atributos = new List<Atributo>();
+                            padre.hijas.Add(ret);
+                            context.categorias.Add(ret);
+                            context.SaveChanges();
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        bool AgregarCategoriaSimple(string idCategoria, string idPadre)
+        //idPadre no puede ser null.
+        //Devuelve FALSE si ya existe una Categoria con el mismo nombre o no existe el padre.
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+                    var existeCat = from c in context.categorias
+                                    where c.CategoriaID == idCategoria
+                                    select c;
+                    if (existeCat != null)
+                        return false;
+                    else
+                    {
+                        var existePadre = from p in context.categorias
+                                          where p.CategoriaID == idPadre
+                                          select p;
+                        if (existePadre == null)
+                            return false;
+                        else
+                        {
+                            CategoriaCompuesta padre = (CategoriaCompuesta)existePadre.FirstOrDefault();
+                            CategoriaSimple ret = new CategoriaSimple();
+                            ret.CategoriaID = idCategoria;
+                            ret.padre = padre;
+                            ret.productos = new List<Producto>();
+                            ret.atributos = new List<Atributo>();
+                            padre.hijas.Add(ret);
+                            context.categorias.Add(ret);
+                            context.SaveChanges();
+                            return true;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
+
+        //CU: 1.4 INGRESAR TIPO DE ATRIBUTO Y 1.5 ALTA ATRIBUTO
+        void AgregarAtributo(string idCategoria, string idAtributo, string valor)
+        {
+            using (var context = new ChebayDBContext())
+            {
+                try
+                {
+
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine(e.Message);
+                }
+            }
+        }
+
     }
 }
--- ConsoleApplication1/IDALTienda.cs
+++ ConsoleApplication1/IDALTienda.cs
@@ -10,32 +10,56 @@ namespace DataAccessLayer
 {
     interface IDALTienda
     {
+        // -- ADMINISTRADORES --
+
         void AgregarAdministrador(Administrador a);
 
         //void ModificarAdministrador(Administrador a);
 
         Administrador ObtenerAdministrador(string idAdministrador);
 
-        Administrador ObtenerAdministradorTienda(int idTienda);
-
+        List<Administrador> ObtenerAdministradoresTienda(int idTienda);
+    
+    //CU: 1.1 INICIAR SESIÓN BACK OFFICE
         bool AutenticarAdministrador(string idAdministrador, string passwd);
         //Devuelve true si es el password correcto para el usuario.
 
         bool CambiarPassAdministrador(string idAdministrador, string passwdVieja, string passwdNueva);
         //Devuelve true si passwdVieja es la password del idAdministrador y pudo cambiarla por passwdNueva.
 
-        void AgregarTienda(Tienda t);
+        // -- TIENDAS --
+
+        bool AgregarTienda(Tienda t);
+        //Completa el nombre, descripción, una URL (TiendaID).
+        //Devuelve FALSE si ya existe una Tienda con la misma URL.
 
         void ActualizarTienda(Tienda t);
+        //Cambiar nombre o descripción de t.
 
-        Tienda ObtenerTienda(int id);
+        bool CambiarURLTienda(string idTienda, string nuevaURL);
+        //Cambia la URL de idTienda por nuevaURL.
+        //Devuelve FALSE si ya existe una tienda con URL nuevaURL.
 
-        Tienda ObtenerTiendaURL(string url);
-
-        Tienda ObtenerTiendaTitulo(string titulo);
+        Tienda ObtenerTienda(string idTienda);
 
         List<Tienda> ObtenerTodasTiendas();
 
-        Tienda ObtenerTiendasAdministrador(string username);
+        Tienda ObtenerTiendasAdministrador(string idAdministrador);
+
+        void AgregarAdminTienda(string idAdministrador, string idTienda);
+        //Agrega a idAdministrador a idTienda.
+        
+        // -- CATEGORIAS --
+
+    //CU: 1.2 INGRESAR CATEGORIA Y 1.3 ALTA CATEGORIA
+        bool AgregarCategoriaCompuesta(string idCategoria, string idPadre);
+        //idPadre no puede ser null. La categoría raiz se crea cuando se crea la tienda.
+        //Devuelve FALSE si ya existe una Categoria con el mismo nombre.
+        bool AgregarCategoriaSimple(string idCategoria, string idPadre);
+        //idPadre no puede ser null.
+        //Devuelve FALSE si ya existe una Categoria con el mismo nombre.
+
+    //CU: 1.4 INGRESAR TIPO DE ATRIBUTO Y 1.5 ALTA ATRIBUTO
+        void AgregarAtributo(string idCategoria, string idAtributo, string valor);
     }
 }

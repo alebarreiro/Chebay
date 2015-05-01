5531879768d8cb01d56d76aab3763c7e1d2461c6
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-04-30 22:47:16 -0300

    Nuevo frontoffice. Listado de productos. Mas diseño

--- Chebay.Frontoffice/Controllers/HomeController.cs
+++ Chebay.Frontoffice/Controllers/HomeController.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAccessLayer;
+using Shared.DataTypes;
 
 namespace Chebay.Frontoffice.Controllers
 {
     public class HomeController : Controller
     {
+        IDALSubasta controladorSubasta = new DALSubastaEF();
+
         public ActionResult Index(string urlTienda)
         {
             if (urlTienda != null) {
                 ViewBag.Message = urlTienda;
-
+                
             }
             else
             {
                 ViewBag.Message = "Ingresa /{nombreTienda}";
             }
+            List<DataProducto> prods = controladorSubasta.ObtenerProductosPersonalizados("Tienda1");
+            ViewBag.productos = prods;
             return View();
         }
 
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Frontoffice.Startup))]
+namespace Frontoffice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

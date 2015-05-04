90352b08ffac40a3b123f90c051c58de47da3f76
leokraken <joleocl@gmail.com>
2015-05-03 20:29:53 -0700

    Ejemplo creacion categoria Program.cs (comentado)

--- Chebay.DataAccessLayer/Program.cs
+++ Chebay.DataAccessLayer/Program.cs
@@ -23,34 +23,54 @@ namespace DataAccessLayer
                 //ChebayDBPublic.ProvidePublicSchema();
                 //ChebayDBContext.ProvisionTenant("PhoneBay");
 
-///                using (var schema = ChebayDBContext.CreateTenant("PhoneBay"))
-   //             {
-     //               schema.seed();
-            //    }
+                using (var schema = ChebayDBContext.CreateTenant("PhoneBay"))
+                {
+                    //var query = from cat in schema.categorias
+                    //            where cat.CategoriaID ==1
+                    //            select cat;
+                    //CategoriaCompuesta father =  (CategoriaCompuesta)query.FirstOrDefault();
+                    //System.Console.WriteLine(father.CategoriaID+father.Nombre);
+                    //Categoria c = new CategoriaCompuesta { Nombre="testing", padre=father};
+                    //schema.categorias.Add(c);
+                    //schema.SaveChanges();
+                    var query = from cat in schema.categorias
+                                where cat.CategoriaID == 1
+                                select cat;
+                    CategoriaCompuesta c = (CategoriaCompuesta)query.FirstOrDefault();
+                    foreach (var hijos in c.hijas)
+                    {
+                        System.Console.WriteLine(hijos.CategoriaID+hijos.Nombre);
+                    }
+                    Console.Read();
+
+                    Console.Read();
+
+                    //schema.seed();
+                }
         //
             
-                using (var schema = ChebayDBPublic.CreatePublic())
-                {
-                    IDALTienda it = new DALTiendaEF();
-                   // it.AgregarAdministrador("idAdmin2", "pass123");
-                    System.Console.WriteLine("ID      pass");
-                    foreach (var a in schema.administradores.ToList())
-                    {
-                        System.Console.WriteLine(a.AdministradorID + "     " + a.password);
-                    }
+                //using (var schema = ChebayDBPublic.CreatePublic())
+                //{
+                //    IDALTienda it = new DALTiendaEF();
+                //   // it.AgregarAdministrador("idAdmin2", "pass123");
+                //    System.Console.WriteLine("ID      pass");
+                //    foreach (var a in schema.administradores.ToList())
+                //    {
+                //        System.Console.WriteLine(a.AdministradorID + "     " + a.password);
+                //    }
                     
-                    foreach (var a in schema.tiendas.ToList())
-                    {
-                        System.Console.Write(a.TiendaID +
-                            "     " + a.nombre +
-                            "   " + a.descripcion);
-                        foreach (var adm in a.administradores.ToList())
-                        {
-                            System.Console.WriteLine("    " + adm.AdministradorID);
-                        }
-                    }
-                    System.Console.ReadLine();
-                }
+                //    foreach (var a in schema.tiendas.ToList())
+                //    {
+                //        System.Console.Write(a.TiendaID +
+                //            "     " + a.nombre +
+                //            "   " + a.descripcion);
+                //        foreach (var adm in a.administradores.ToList())
+                //        {
+                //            System.Console.WriteLine("    " + adm.AdministradorID);
+                //        }
+                //    }
+                //    System.Console.ReadLine();
+                //}
             
 
             

0eca7875c302e0caee09e4cdcaae484fbd1042e7
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-05-05 16:34:37 -0300

    ahora se hace la oferta

--- WebApplication1/Hubs/SubastaHub.cs
+++ WebApplication1/Hubs/SubastaHub.cs
@@ -5,14 +5,25 @@ using System.Web;
 using Microsoft.AspNet.SignalR;
 using System.Web.Script.Serialization;
 using System.Diagnostics;
+using DataAccessLayer;
+using Shared.Entities;
 
 namespace WebApplication1.Hubs
 {
     public class SubastaHub : Hub
     {
 
-        public void PlaceNewBid(int productId, int newBid)
+        public void PlaceNewBid(int productId, int newBid, bool _esFinal, string userId)
         {
+            IDALSubasta controladorSubasta = new DALSubastaEF();
+
+            Oferta o = new Oferta { 
+                esFinal = _esFinal,
+                monto = newBid,
+                ProductoID = productId,
+                UsuarioID = userId
+            };
+            controladorSubasta.OfertarProducto(o, "TestURL");
             Clients.All.newBidPosted(productId, newBid);
         }
 

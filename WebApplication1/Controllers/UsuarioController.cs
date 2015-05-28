0afcfd2ad466f12bae5a622e15af565487b665ec
Alejandro Barreiro <alebarreiro91@gmail.com>
2015-05-27 21:49:14 -0300

    cambiar usuario a datausuario

--- WebApplication1/Controllers/UsuarioController.cs
+++ WebApplication1/Controllers/UsuarioController.cs
@@ -13,24 +13,36 @@ namespace WebApplication1.Controllers
 {
     public class UsuarioController : Controller
     {
-        private Shared.Entities.Usuario _usuarioLoggeado;
 
         IDALUsuario uC = new DALUsuarioEF();
 
-        public UsuarioController()
+        public class DatosUsuarioFull
         {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Pais { get; set; }
+            public string Ciudad { get; set; }
+            public string Direccion { get; set; }
+            public string NumeroContacto { get; set; }
+            public int CodigoPostal { get; set; }
+            public string Email { get; set; }
+            public int compras_valor { get; set; }
+            public int ventas_valor { get; set; }
+            public double promedio_calificacion { get; set; }
+            public List<DataProductoUsuario> publicados { get; set; }
+            public List<DataCompraUsuario> compras { get; set; }
         }
 
-        public Shared.Entities.Usuario usuarioLoggeado
+        public class DataProductoUsuario
         {
-            get
-            {
-                return _usuarioLoggeado;
-            }
-            set
-            {
-                _usuarioLoggeado = value;
-            }
+            public long ProductoID { get; set; }
+            public string nombre { get; set; }
+        }
+
+        public class DataCompraUsuario
+        {
+            public long ProductoID { get; set; }
+            public int monto { get; set; }
         }
 
         // GET: Usuario
@@ -38,28 +50,65 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             String tienda = Session["Tienda_Nombre"].ToString();
-            Debug.WriteLine(tienda);
-            if (Session["Usuario"] != null)
-            {
-                Usuario u = (Usuario)Session["Usuario"];
-                ViewBag.usuario = u;
-            }
-            else
-            {
                 String userName = User.Identity.GetUserName();
                 try
                 {
-                    Usuario u = uC.ObtenerUsuario(userName, Session["Tienda_Nombre"].ToString());
-                    Session["Usuario"] = u;
+                    Usuario u = uC.ObtenerUsuarioFull(userName, Session["Tienda_Nombre"].ToString());
+                    DatosUsuarioFull duf = new DatosUsuarioFull
+                    {
+                        Nombre = u.Nombre,
+                        Apellido = u.Apellido,
+                        Pais = u.Pais,
+                        Ciudad = u.Ciudad,
+                        Direccion = u.Direccion,
+                        NumeroContacto = u.NumeroContacto,
+                        CodigoPostal = u.CodigoPostal,
+                        Email = u.Email,
+                        compras_valor = u.compras_valor,
+                        ventas_valor = u.ventas_valor,
+                        promedio_calificacion = u.promedio_calificacion
+                    };
+                    List<DataProductoUsuario> dpus = new List<DataProductoUsuario>();
+                    if (u.publicados != null)
+                    {
+                        foreach (Producto p in u.publicados)
+                        {
+                            DataProductoUsuario dpu = new DataProductoUsuario
+                            {
+                                ProductoID = p.ProductoID,
+                                nombre = p.nombre
+                            };
+                            dpus.Add(dpu);
+                        }
+                    }
+                    
+                    duf.publicados = dpus;
+
+                    List<DataCompraUsuario> dcus = new List<DataCompraUsuario>();
+                    if (u.compras != null)
+                    {
+                        foreach (Compra c in u.compras)
+                        {
+                            DataCompraUsuario dcu = new DataCompraUsuario
+                            {
+                                ProductoID = c.ProductoID,
+                                monto = c.monto
+                            };
+                            dcus.Add(dcu);
+                        }
+                    }
+                    
+                    duf.compras = dcus;
+
                     ImagenUsuario iu = uC.ObtenerImagenUsuario(userName, tienda);
+                    //ImagenUsuario iu = u.Imagen;
                     ViewBag.imagenUsuario = iu.Imagen;
-                    ViewBag.usuario = u;
+                    ViewBag.usuario = duf;
                 }
                 catch (Exception e)
                 {
                     Debug.WriteLine(e.Message);
                 }
-            }
             
             return View();
         }
